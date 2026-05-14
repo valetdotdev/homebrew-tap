87d526ac26c2f9336018bb6aad0b052f8ce5f4ce
Jamu Kakar <jkakar@valet.dev>
2026-05-13 17:50:57 -0700

    Update valet to v0.1.52 (#70)
    Update valet Homebrew formula to v0.1.52.

--- Formula/valet.rb
+++ Formula/valet.rb
@@ -1,25 +1,25 @@
 class Valet < Formula
   desc "Valet runs your agents"
   homepage "https://valet.dev"
-  version "0.1.51"
+  version "0.1.52"
 
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/valetdotdev/homebrew-tap/releases/download/valet-cli-v#{version}/valet-cli-#{version}-darwin-arm64.tar.gz"
-      sha256 "feb27056c20c2bae4e69b0537ccd5e3cc2fd658816713961afb7a95f590a5f16"
+      sha256 "73459b5cf248e6d35d7b0867e705dd9482390a5c660899be2fe6fe92d2dd9464"
     else
       url "https://github.com/valetdotdev/homebrew-tap/releases/download/valet-cli-v#{version}/valet-cli-#{version}-darwin-amd64.tar.gz"
-      sha256 "0d55e77e3607f2379e06488d8a35329d24d3a69cc023216453b03948d08fbd52"
+      sha256 "66757deefe3b61bc0c9a750246475cce37167119faee2d0592050f87dd0d74b0"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/valetdotdev/homebrew-tap/releases/download/valet-cli-v#{version}/valet-cli-#{version}-linux-arm64.tar.gz"
-      sha256 "71bd469cb20526185d6afb62353bcf0e421e54de39d3f5080b4912a5df50ef8a"
+      sha256 "c66e9cfd1421b366db867299d1e331409a49bb014e5018791e933e96f200bd74"
     else
       url "https://github.com/valetdotdev/homebrew-tap/releases/download/valet-cli-v#{version}/valet-cli-#{version}-linux-amd64.tar.gz"
-      sha256 "9b67d20ee2bac6e30fd5ca85d611add67510f461f9eef3e2b93cc1f2e58cdc8e"
+      sha256 "092349e7ab7a1dd2267e5bc80f6be95a767ff58fcde9413b274b7e7038165f6f"
     end
   end
 
@@ -28,6 +28,6 @@ class Valet < Formula
   end
 
   test do
-    assert_match "0.1.51", shell_output("#{bin}/valet version")
+    assert_match "0.1.52", shell_output("#{bin}/valet version")
   end
 end
